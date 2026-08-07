8f26f5a3c34fef35e68b84a690049e5b0cd82d10
blackySascha <asschwarz054@gmail.com>
2026-08-07 12:06:46 +0200

    v0.3.0 calculator with two numbers and /*-+

--- main.py
+++ main.py
@@ -1,56 +1,71 @@
 #Blacky Assisstant v0.2.0
 import tkinter as tk
 import tkinter
+
 def calculator():
     global button_calculator
-    global button_notes
-    global button_terminal
-    global button_information
-    global startba
-    global answer
-    button_information.destroy()
-    button_terminal.destroy()
     button_calculator.destroy()
-    button_notes.destroy()
-    def firstnumber():
-        global number1
-        entry_no1 = tk.Entry(window,width=10,bg="black",fg="white")
-        entry_no1.place(x=100,y=100)
-        number1 = entry_no1.get()
-    def secondnumber():
-        global number2
-        entry_no2 = tk.Entry(window,width=10,bg="black",fg="white")
-        entry_no2.place(x=200,y=100)
-        number2 = entry_no2.get()
-    def multiplication():
-        global answer
-        answer = number1 * number2
-    def division():
-        global answer
-        answer = number1 / number2
-    def addition():
-        global answer
-        answer = number1 + number2
-    def subtraction():
-        global answer
-        answer = number1 - number2
-    button_multi = tkinter.Button(window,text="*",width=10,bg="black",fg="white",command=multiplication)
-    button_multi.place(x=100,y=200)
-    button_sub = tkinter.Button(window,text="-",width=10,bg="black",fg="white",command=subtraction)
-    button_sub.place(x=150,y=200)
-    button_add = tk.Button(window,text="+",width=10,bg="black",fg="white",command=addition)
-    button_add.place(x=200,y=200)
-    button_div = tk.Button(window,text="/",width=10,bg="black",fg="white",command=division)
-    button_div.place(x=250,y=200)
-    label_answer = tk.Label(window,text=f"Answer:{answer}")
-    label_answer.place(x=300,y=100)
-def startba():
-    global button_start
-    global button_calculator
     global button_notes
-    global button_terminal
+    button_notes.destroy()
     global button_information
-    global startba
+    button_information.destroy()
+    global button_terminal
+    button_terminal.destroy()
+    def get_num1():
+        num1 = int(text_num1.get())
+        return num1
+    def get_num2():
+        num2 = int(text_num2.get())
+        return num2
+    def set_answer(result):
+        text_answer.delete(0, "end")
+        text_answer.insert(0, result)
+    def add():
+        num1 = get_num1()
+        num2 = get_num2()
+        result = num1 + num2
+        set_answer(result)
+    def sub():
+        num1 = get_num1()
+        num2 = get_num2()
+        result = num1 - num2
+        set_answer(result)
+    def mul():
+        num1 = get_num1()
+        num2 = get_num2()
+        result = num1 * num2
+        set_answer(result)
+    def div():
+        num1 = get_num1()
+        num2 = get_num2()
+        result = num1 / num2
+        set_answer(result)
+    button_add = tkinter.Button(window, text="+", command=add, width=10, height=2, bg="gold")
+    button_sub = tkinter.Button(window, text="-", command=sub, width=10, height=2, bg="gold")
+    button_mul = tkinter.Button(window, text="*", command=mul, width=10, height=2, bg="gold")
+    button_div = tkinter.Button(window, text="/", command=div, width=10, height=2, bg="gold")
+    text_num1 = tkinter.Entry(window, width=24, bg="black", fg="gold")
+    text_num2 = tkinter.Entry(window, width=24, bg="black", fg="gold")
+    text_answer = tkinter.Entry(window, width=24, bg="black", fg="gold")
+    label_num1 = tkinter.Label(window, text="Enter the first number",width=20, background="silver")
+    label_num2 = tkinter.Label(window, text="Enter the second number",width=20, background="silver")
+    label_answer = tkinter.Label(window, text="Operation result:",width=20, background="silver")
+    label_num1.place(x=200, y=80)
+    label_num2.place(x=200, y=120)
+    label_answer.place(x=200, y=280)
+    text_num1.place(x=200, y=100)
+    text_num2.place(x=200, y=140)
+    button_add.place(x=200, y=180)
+    button_sub.place(x=270, y=180)
+    button_mul.place(x=200, y=220)
+    button_div.place(x=270, y=220)
+    text_answer.place(x=200, y=300)
+
+def startba():
+    global button_notes
+    global button_information
+    global button_terminal
+    global button_calculator
     button_start.destroy()
     button_calculator = tk.Button(window,text="Calculator",width=10,bg="black",fg="white",command=calculator)
     button_calculator.place(x=100,y=100)
